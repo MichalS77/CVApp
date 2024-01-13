42811bdfaeada5fc0de75c2d08f644deba00f068
Michal S <minitrikus@gmail.com>
2024-01-13 15:59:11 +0100

    General Info component

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import MyForm from './General Information.jsx'
+MyForm
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <MyForm />
   </React.StrictMode>,
 )
